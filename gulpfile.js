f541014c780be21b8e7312027cb2b165ff65384a
Anton <vasya@example.com>
2016-06-02 21:08:23 +0300

    gulp lesson second

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,43 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var includer = require('gulp-htmlincluder');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+var cleanCss = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('missionZero', function(){
+	connect.server({
+		root: 'build/',
+		livereload: true
+	});
+});
+
+
+
 
 gulp.task('missionOne', function(){
-	gulp.src('dev/*.css').pipe(concatCss('style.css')).pipe(gulp.dest('build/css/'));
+	gulp.src('dev/*.css')
+	.pipe(concatCss('style.css'))
+	.pipe(autoprefixer({
+		browsers: ['> 5%', 'IE 7'],
+			cascade: false
+	}))
+	.pipe(cleanCss({
+		compability: 'ie8'
+	}))
+	.pipe(gulp.dest('build/css/'))
+	.pipe(connect.reload());
 
 	
 });
 gulp.task('missionTwo', function(){
-	gulp.src('dev/html/*.html').pipe(includer()).pipe(gulp.dest('build/'));
-
-	
+	gulp.src('dev/html/**/*.html').pipe(includer()).pipe(gulp.dest('build/')).pipe(connect.reload());
 });
 
 
 gulp.task('default', function(){
-	gulp.start('missionOne', 'missionTwo');
+	gulp.start('missionOne', 'missionTwo', 'missionZero');
 
 	gulp.watch(['dev/**/*.css'], function(){
 		gulp.start('missionOne');
@@ -24,3 +46,4 @@ gulp.task('default', function(){
 		gulp.start('missionTwo');
 	});
 });
+
